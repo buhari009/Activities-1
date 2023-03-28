2d43d051f591746e8157cf4b52dd01cd862213a1
Aryan Saxena <40167579+AryanSaxena16@users.noreply.github.com>
2023-03-28 16:08:19 +0530

    Create SavingAccount.cs

---
+++ Week 4 Day 5 : Understanding C# Inheritance/Que 4/SavingAccount.cs
@@ -0,0 +1,9 @@
+using System;
+public class SavingAccount : Account
+{
+    public double _minimumBalance;
+    public SavingAccount(string accountNumber, double balance, string accountHolderName, double minimumBalance) : base(accountNumber, balance, accountHolderName)
+    {
+        _minimumBalance = minimumBalance;
+    }
+}
