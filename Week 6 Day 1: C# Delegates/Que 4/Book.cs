0774fc5f5d4f2182b29a5681fd265b616d3409d4
Aryan Saxena <40167579+AryanSaxena16@users.noreply.github.com>
2023-04-03 21:41:48 +0530

    Que 4

---
+++ Week 6 Day 1: C# Delegates/Que 4/Book.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Library
+{
+    class Book
+    {
+        private string _title;
+        private string _author;
+        private double _amount;
+        private int _noOfPages;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        public int NoOfPages
+        {
+            get { return _noOfPages; }
+            set { _noOfPages = value; }
+        }
+
+        public Book(string title, string author, double amount, int noOfPages)
+        {
+            _title = title;
+            _author = author;
+            _amount = amount;
+            _noOfPages = noOfPages;
+        }
+
+        public int CompareBook(Book book1, Book book2)
+        {
+            return book1.Title.CompareTo(book2.Title);
+        }
+    }
+}
