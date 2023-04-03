2e469474198bb2909f5fc868bad12e77d5393c73
Aryan Saxena <40167579+AryanSaxena16@users.noreply.github.com>
2023-04-03 09:57:02 +0530

    Que 11

---
+++ Week 5 Day 5 : C# - Generics/Que 11/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the input :");
+        var input = Console.ReadLine();
+        GenericClass.Print(input);
+    }
+}
